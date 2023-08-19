d4a41f982005769699d485c1cebef57c811b2391
Myles Joseph <125922096+M-Joseph29@users.noreply.github.com>
2023-08-19 17:54:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://smart-brain-1emm.onrender.com', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
